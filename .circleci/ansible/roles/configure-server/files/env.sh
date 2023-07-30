bf238e07c14bf65773de68c089f1246573062cea
Purva Tomar <purvato@amdocs.com>
2023-07-30 23:36:47 +0530

    updating project

--- .circleci/ansible/roles/configure-server/files/env.sh
+++ .circleci/ansible/roles/configure-server/files/env.sh
@@ -4,6 +4,6 @@ export TYPEORM_HOST: database-1.cr7zx20upy6r.us-east-1.rds.amazonaws.com
 export TYPEORM_PORT: 5432
 export TYPEORM_USERNAME: postgres
 export TYPEORM_PASSWORD: postgres
-export TYPEORM_DATABASE: udapeople
+export TYPEORM_DATABASE: postgres
 export TYPEORM_MIGRATIONS: ./src/migrations/*.ts
 export TYPEORM_MIGRATIONS_DIR: ./src/migrations
